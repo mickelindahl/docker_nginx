899c0cdcfe6812a7deee73e65ddefffb6b81fe8a
Mikael Lindahl <apps@debian>
2017-05-15 10:05:28 +0000

    fixes

--- copy_cert.sh
+++ copy_cert.sh
@@ -3,7 +3,12 @@
 # $1 - domain/subdomain
 # $2 - app root
 
+echo "In copy_cert.sh"
+
 # nginx need the fullchain to work properly in all browsers
+echo "Copy cert /etc/letsencrypt/live/$1/fullchain.pem $2/conf/certs/$1.crt"
 cp /etc/letsencrypt/live/$1/fullchain.pem $2/conf/certs/$1.crt
+
+echo "Copy key /etc/letsencrypt/live/$1/privkey.pem $2/conf/certs/$1.key"
 cp /etc/letsencrypt/live/$1/privkey.pem $2/conf/certs/$1.key
 
