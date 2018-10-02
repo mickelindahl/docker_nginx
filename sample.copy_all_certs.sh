38c91fea90d326456ce641fcff2c8724c5e1437c
Mikael Lindahl <mikael.lindahl@greencago.com>
2018-10-02 11:11:23 +0200

    fix

--- sample.copy_all_certs.sh
+++ sample.copy_all_certs.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-echo "Set HOME={path-nginx}"
-export HOME={path-nginx}
+echo "Set HOME_NGINX={path-nginx}"
+export HOME_NGINX={path-nginx}
 
 echo "Declare array"
 #declare -a arr=("{domain/subdomain}" \
 #		"{subdomain}" \
 #		"... etc")
 
-if [ ! -f virtual-hosts ]; then
+if [ ! -f $HOME_NGINX/virtual-hosts ]; then
   echo "Missing virtual-host file. Please create it. See README.md"
   exit
 fi 
@@ -20,7 +20,7 @@ for host in $VIRTUAL_HOSTS
 do
 
   echo "Copying cert and key for $host"
-  $HOME/copy_cert.sh $host $HOME
+  $HOME_NGINX/copy_cert.sh $host $HOME_NGINX
 
    # or do whatever with individual element of the array
 done
