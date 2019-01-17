cd06261508fc1de3a1d9c8ba4bf1676018b905f7
Mikael Lindahl <mikael.lindahl@greencargo.com>
2019-01-17 12:22:29 +0100

    renew certs fixes

--- install_cron.sh
+++ install_cron.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 cp sample.nginx-cron nginx-cron
+cp sample.renew-certs.sh renew-certs.sh
 
 sed -i "s#{path-nginx}#$(pwd)#g" nginx-cron
+sed -i "s#{path-nginx}#$(pwd)#g" renew-certs.sh
 
 mv nginx-cron /etc/cron.d
 
--- sample.renew-certs.sh
+++ sample.renew-certs.sh
@@ -1,34 +1,8 @@
 #!/bin/bash
 
-echo "Set HOME={path-nginx}"
-export HOME={path-nginx}
+echo `date`
 
-echo "Declare array"
-#declare -a arr=("{domain/subdomain}" \
-#		"{subdomain}" \
-#		"... etc")
-
-if [ ! -f "virtual-hosts" ]; then
-  echo "Missing virtual-host file. Please create it. See README.md"
-  exit
-fi
-
-#docker nginx stop
-certbot renew
-
-VIRTUAL_HOSTS=sudo cat $HOME/virtual-hosts | tr '\n' ' '
-
-echo "Loop through the above array"
-for host in $VIRTUAL_HOSTS
-do
-
-  echo "Copying cert and key for $host"
-  sudo $HOME/copy_cert.sh $host $HOME
-
-   # or do whatever with individual element of the array
-done
-
-echo "Clearing previous conf"
-sudo rm $HOME/conf/conf.d/default.conf
-
-#docker nginx start
+docker stop nginx 
+certbot renew 
+{path-nginx}/copy_all_certs.sh
+docker start nginx
