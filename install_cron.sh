25dea0083099f2bf7e3f8a46186245a76b2fb027
mickelindahl <mikael.lindahl.07@gmail.com>
2019-02-08 11:48:00 +0000

    local stuff

--- install_cron.sh
+++ install_cron.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 cp sample.nginx-cron nginx-cron
+cp sample.renew.sh renew.sh
 
 sed -i "s#{path-nginx}#$(pwd)#g" nginx-cron
+sed -i "s#{path-nginx}#$(pwd)#g" renew.sh
 
 mv nginx-cron /etc/cron.d
 
